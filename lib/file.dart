df40d7f6f7b70a36c173de40355b57ade257cd74
Mahalakshmi <maha18aaru@gmail.com>
2024-08-29 18:38:34 +0530

    file created

---
+++ lib/file.dart
@@ -0,0 +1,7 @@
+import 'package:file_comparison_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+
+void main() {
+  runApp(const FileComparisonApp());
+}
